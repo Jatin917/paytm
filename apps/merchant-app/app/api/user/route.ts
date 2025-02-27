6f5d561810e782347163f54cd038b9f2e259f277
Jatin917 <jkchandel02@gmail.com>
2025-02-28 04:34:29 +0530

    removed default adding user

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -4,14 +4,6 @@ import prisma from '@repo/db/client';
 
 
 export const GET = async () => {
-    await prisma.user.create({
-        data: {
-            email: "jatin",
-            name: "adsads",
-            number:"123654689",
-            password:"555555"
-        }
-    })
     return NextResponse.json({
         message: "hi there"
     })
