9bd535ad0dc338391d990977f381f95867b87b67
Jatin917 <jkchandel02@gmail.com>
2025-02-28 05:01:44 +0530

    improved changes code readeablity

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,8 +1,4 @@
 import { NextResponse } from "next/server"
-import prisma from '@repo/db/client';
-
-
-
 export const GET = async () => {
     return NextResponse.json({
         message: "hi there"
